f0d6245bd076c7ab6e26a8bc04b0d2d9a8b2752a
Rey Delos Reyes <reyjoshua5922@gmail.com>
2024-12-16 15:07:34 +0800

    tanginamo ken

--- Tite.java
+++
@@ -1,9 +0,0 @@
-public class Tite {
-    public static void main(String[] args) {
-        System.out.println(" ###### ######");
-        for (int i = 0; i < 5; i++) System.out.println("###############");
-        System.out.println(" #############");
-        for (int i = 0; i < 15; i++) System.out.println("  ###########");
-        System.out.println("#############");
-    }
-}
