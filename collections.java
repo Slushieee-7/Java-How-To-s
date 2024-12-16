7894ee7359c4306a9a8eebc3820f83cd6080f5ad
Rey Delos Reyes <reyjoshua5922@gmail.com>
2024-12-17 07:57:41 +0800

    fix minor issues

--- collections.java
+++ collections.java
@@ -1,73 +1,74 @@
 import java.util.*;
-class collections {
 
-    public void arraylist(int x, int y){
+class CollectionsExample { // Renamed class to follow Java naming conventions
+
+    public void arrayList(int x, int y) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(x);
         list.add(y);
-        Iterator itr = list.iterator();
-        while(itr.hasNext()){
-            System.out.println(itr.next());
+        // Using enhanced for loop for better readability
+        for (Integer num : list) {
+            System.out.println(num);
         }
     }
 
-    public void linkedlist(int x, int y){
-        LinkedList<Integer> al = new LinkedList<>();
-        al.add(x);
-        al.add(y);
-        Iterator it = al.iterator();
-        while (it.hasNext()) {
-        System.out.println(it.next());
-        } 
+    public void linkedList(int x, int y) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(x);
+        list.add(y);
+        // Using enhanced for loop for better readability
+        for (Integer num : list) {
+            System.out.println(num);
+        }
     }
 
-    public void vector(int x, int y){
+    public void vector(int x, int y) {
         Vector<Integer> v = new Vector<>();
         v.add(x);
         v.add(y);
-        Iterator it = v.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
+        // Using enhanced for loop for better readability
+        for (Integer num : v) {
+            System.out.println(num);
         }
     }
 
     public static void main(String[] args) {
-        Scanner choi = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("\t Pick a collection: \n\t 1. ArrayList \n\t 2. LinkedList \n\t 3. Vector \n: ");
-        int choice = choi.nextInt();
+        int choice = scanner.nextInt();
 
-        switch (choice){
+        switch (choice) {
             case 1:
-            System.out.println(" Enter the first number you want to add in the ArrayList: ");
-            int a = choi.nextInt();
-            System.out.println(" Enter the second number you want to add in the ArrayList: ");
-            int b = choi.nextInt();
-            collections arraylist = new collections();
-            arraylist.arraylist(a, b);
-            break;
+                System.out.println("Enter the first number you want to add in the ArrayList: ");
+                int a = scanner.nextInt();
+                System.out.println("Enter the second number you want to add in the ArrayList: ");
+                int b = scanner.nextInt();
+                CollectionsExample arrayListExample = new CollectionsExample();
+                arrayListExample.arrayList(a, b);
+                break;
 
             case 2:
-            System.out.println(" Enter the first number you want to add in the LinkedList: ");
-            int c = choi.nextInt();
-            System.out.println(" Enter the second number you want to add in the LinkedList: ");
-            int d = choi.nextInt();
-            collections linkedlist = new collections();
-            linkedlist.arraylist(c, d);
-            break;
+                System.out.println("Enter the first number you want to add in the LinkedList: ");
+                int c = scanner.nextInt();
+                System.out.println("Enter the second number you want to add in the LinkedList: ");
+                int d = scanner.nextInt();
+                CollectionsExample linkedListExample = new CollectionsExample();
+                linkedListExample.linkedList(c, d); // Fixed method call
+                break;
 
             case 3:
-            System.out.println(" Enter the first number you want to add in the Vector: ");
-            int e = choi.nextInt();
-            System.out.println(" Enter the second number you want to add in the Vector: ");
-            int f = choi.nextInt();
-            collections vector = new collections();
-            vector.vector(e, f);
-            break;
+                System.out.println("Enter the first number you want to add in the Vector: ");
+                int e = scanner.nextInt();
+                System.out.println("Enter the second number you want to add in the Vector: ");
+                int f = scanner.nextInt();
+                CollectionsExample vectorExample = new CollectionsExample();
+                vectorExample.vector(e, f);
+                break;
 
             default:
-            System.out.println("Invalid choice");
-            break;
+                System.out.println("Invalid choice");
+                break;
         }
-        choi.close();
+        scanner.close(); // Closed the scanner
     }
 }
