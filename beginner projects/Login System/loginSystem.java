c0a1a73cff6c957f7b14817b87b3813add85716a
Rey Delos Reyes <reyjoshua5922@gmail.com>
2024-12-22 14:24:01 +0800

    to be continued (chores)

--- beginner projects/Login System/loginSystem.java
+++ beginner projects/Login System/loginSystem.java
@@ -1,8 +1,14 @@
+//we make different classes that are placed on the same folder for much cleaner code
+
 import java.util.HashMap;
 
 public class loginSystem {
-
+    
     public static void main(String[] args) {
-        
+
+        //creating instances for the different classes
+        IDandPasswords idandPass = new IDandPasswords();
+        LoginPage loginpage = new LoginPage(idandPass.getInfo());
+        WelcomePage welcome = new WelcomePage();
     }
 }
