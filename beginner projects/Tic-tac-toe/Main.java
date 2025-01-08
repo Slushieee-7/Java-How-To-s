c46103200a5446814d7f82c94ba0461c7b682062
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-08 13:30:58 +0800

    new proj

---
+++ beginner projects/Tic-tac-toe/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
---
+++ beginner projects/Tic-tac-toe/TicTacToe.java
@@ -0,0 +1,21 @@
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class TicTacToe implements ActionListener{
+    
+    TicTacToe() { 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void first(){
+        
+    }
+}
