b36af76caf4b57c3687d91b3f9ee3ecfb646173b
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-12 00:01:33 +0800

    update

--- W3schools examples/Random Number/random.java
+++ W3schools examples/Random Number/random.java
@@ -5,8 +5,8 @@ public class random {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
         double number = input.nextDouble();
-        double rnd = Math.random();
-        System.out.println("You have guessed the right number! \n Guessed number: " + number + "\n" + "Random number: " + rnd);
+        System.out.println("Your guess: " + number);
+        System.out.println("Randome number: " + Math.random());
         input.close();
     }
 }
