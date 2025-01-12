71754e016e45cfdd046aaf6cd1890200d05e687f
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-12 17:04:16 +0800

    added frames and panel
    issue: no text displayed

--- beginner projects/Tic-tac-toe/TicTacToe.java
+++ beginner projects/Tic-tac-toe/TicTacToe.java
@@ -1,21 +1,45 @@
 import java.awt.event.*;
 import java.awt.*;
 import java.util.*;
+import javax.swing.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 import javax.swing.event.*;
 
 public class TicTacToe implements ActionListener {
 
+    Random random = new Random();
     JFrame frame = new JFrame();
+    JPanel titlepanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9]; //indication that there wil be 9 slots 
+    boolean player1Turn;
+     
 
     TicTacToe() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 300);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(Color.GRAY);
         frame.setLayout(new BorderLayout());
-        frame.add(new JLabel("Tic Tac Toe"), BorderLayout.NORTH);
         frame.setVisible(true);
-        frame.setLayout(null);
+
+        textfield.setBackground(Color.black);
+        textfield.setForeground(Color.black);
+        textfield.setFont(new Font("Arial", Font.BOLD, 50));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic Tac Toe");
+        textfield.setOpaque(true);
+
+        titlepanel.setLayout(new BorderLayout());
+        titlepanel.setBounds(0, 0, 800, 100);
+        
+        titlepanel.add(textfield);
+        frame.add(titlepanel,BorderLayout.NORTH);
     }
 
     @Override
@@ -24,6 +48,18 @@ public class TicTacToe implements ActionListener {
     }
 
     public void first() {
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(){
 
     }
 }
