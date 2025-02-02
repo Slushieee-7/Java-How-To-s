3083262158b004c898c2483f9bfe3de7ef1ae3e0
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-02-02 20:51:05 +0800

    return palindrome

--- Algorithms in Java/Palindrome check/palindrome.java
+++ Algorithms in Java/Palindrome check/palindrome.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class palindrome {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner val1 = new Scanner(System.in);
         Scanner val2 = new Scanner(System.in);
 
@@ -11,22 +11,38 @@ public class palindrome {
         System.out.println("Enter the second word: ");
         String string2 = val2.nextLine();
 
-        System.out.println(string1 + " is a palindrome: " + isPalindrome(string1));
-        System.out.println(string2 + " is a palindrome: " + isPalindrome(string2));
+        String pal1 = Palindrome(string1);
+        String pal2 = Palindrome(string2);
+
+        System.out.println("\n\n" + string1 + " is a palindrome: " + isPalindrome(string1));
+        System.out.println(string2 + " is a palindrome: " + isPalindrome(string2) + "\n\n");
+        System.out.println("First palindrome " + string1 + " -> " + pal1);
+        System.out.println("First palindrome " + string2 + " -> " + pal2);
 
     }
 
-    public static boolean isPalindrome(String str){
+    public static boolean isPalindrome(String str) {
         int left = 0;
         int right = str.length() - 1;
 
-        while(left < right){
-            if (str.charAt(left) != str.charAt(right)){
-                return false; //if characters are not in the same order, it will return false or not a palindrome
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // if characters are not in the same order, it will return false or not a
+                              // palindrome
             }
             left++;
             right--;
         }
-        return true; //if the loop completes without returning false, it will return true 
+        return true; // if the loop completes without returning false, it will return true
+    }
+
+    //returns the palindrome version of the string
+    public static String Palindrome(String str) {
+        String reverse = " ";
+        for (int i = 0; i < str.length(); i++) {
+            reverse = str.charAt(i) + reverse;
+        }
+        System.out.println(reverse);
+        return reverse;
     }
 }
