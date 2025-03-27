9c84577768dd3e5046aaa1fdd0f099c3cb131fd2
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-03-27 16:29:17 +0800

    tbc

--- beginner projects/Weather-app/src/GUI.java
+++ beginner projects/Weather-app/src/GUI.java
@@ -1,15 +1,32 @@
+import java.awt.Font;
 import javax.swing.*;
 
-public class GUI extends JFrame{
-        public GUI(){
-            //setup our GUI
-            super("Weather App"); //title of our app
+public class GUI extends JFrame {
+    public GUI() {
+        // Setup our GUI
+        super("Weather App"); // Title of our app
 
-            //configuration of GUI 
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setSize(800, 600); //width and height of our app
-            this.setLocationRelativeTo(null); //sets the frame at the center 
-            this.setVisible(true); //makes the frame visible
-            this.setResizable(false);//prevents the window to be resizeable
+        // Configuration of GUI 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(450, 650);//sets the width and height of the app
+        this.setLocationRelativeTo(null);//sets the frame at default center
+        this.setResizable(false);//prevents the window to be resizeable
+
+        addGuiComponents();
+        this.setVisible(true);//makes the frame visible
+    }
+
+    private void addGuiComponents() {
+        //search field for the app
+        JTextField searchField = new JTextField();
+
+        //sets the location and size of our app
+        searchField.setBounds(10, 10, 410, 40);
+
+        //changes the fontstyles and sizes 
+        searchField.setFont(new Font("Arial", Font.BOLD, 24));
+
+        add(searchField);
+        
     }
 }
